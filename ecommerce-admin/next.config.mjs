c7f1b53c334052831d79f7c4590f1fccaaec2e0b
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-03-27 23:44:02 +0100

    commit

--- ecommerce-admin/next.config.mjs
+++ ecommerce-admin/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL, // Definir la URL de NextAuth
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET, // Definir el secreto de NextAuth
+  },
 };
 
 export default nextConfig;
--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -1,18 +1,22 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "../components/Nav.js"
 
 export default function Home() {
   const { data: session } = useSession()
   if (!session) {
     return (
       <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 rounded-lg">Login with Google</button>
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+        </div>
       </div>
-    </div>
     )
   }
 
   return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">Logged In {session.user.email}</div>
+    <div className="bg-blue-900 min-h-screen">
+      <Nav />
+      <div className="">Logged In {session.user.email}</div>
+    </div>
   );
 }
