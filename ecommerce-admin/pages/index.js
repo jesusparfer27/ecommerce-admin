82f460ce977734c0d8b645544ffaacc4514d711a
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-09-05 17:25:03 +0200

    comit

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -4,7 +4,7 @@ import { useSession, signIn } from "next-auth/react"
 
 export default function Home() {
 
-  const { data: session } = useSession();
+  const { data: session } = useSession(); 
   console.log({ session });
   if (!session) {
     return (
